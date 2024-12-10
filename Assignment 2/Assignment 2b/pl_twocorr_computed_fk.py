4758cce40daf8b06b439dd08200dbd5b6f43c802
benjamin <benjamin.elmjonsson@gmail.com>
2024-12-11 00:01:55 +0100

    started fixing 2pointcorr

--- Assignment 2/Assignment 2b/pl_twocorr_computed_fk.py
+++ Assignment 2/Assignment 2b/pl_twocorr_computed_fk.py
@@ -37,8 +37,7 @@ w_time=np.delete(w_time, idelete)
 w_y_z_t= np.reshape(w_time,(nt,nj,nk))
 # swap axis
 w_y_z_t_65= np.swapaxes(w_y_z_t,0,2)  # the order of the indices are (k,j,t)
-
-
+w_y_z_t_65= np.swapaxes(w_y_z_t_65,0,1)
 
 
 # x=0.8
@@ -60,9 +59,7 @@ w_time=np.delete(w_time, idelete)
 w_y_z_t= np.reshape(w_time,(nt,nj,nk))
 # swap axis
 w_y_z_t_80= np.swapaxes(w_y_z_t,0,2)
-
-
-
+w_y_z_t_80= np.swapaxes(w_y_z_t_80,0,1)
 
 # x=1.1 
 w_time = np.loadtxt(name_dat + "w_time_z110.dat")
@@ -83,6 +80,7 @@ w_time=np.delete(w_time, idelete)
 w_y_z_t= np.reshape(w_time,(nt,nj,nk))
 # swap axis
 w_y_z_t_110= np.swapaxes(w_y_z_t,0,2)
+w_y_z_t_110= np.swapaxes(w_y_z_t_110,0,1)
 
 # x=1.3 
 w_time = np.loadtxt(name_dat + "w_time_z130.dat")
@@ -102,8 +100,46 @@ w_time=np.delete(w_time, idelete)
 
 w_y_z_t= np.reshape(w_time,(nt,nj,nk))
 # swap axis
-w_y_z_t_130= np.swapaxes(w_y_z_t,0,2)
+w_y_z_t_130= np.swapaxes(w_y_z_t,0,2) #nk,nj,nt
+w_y_z_t_130= np.swapaxes(w_y_z_t_130,0,1)
 
+B33_norm = np.zeros((4,nj,nk))
+for x in range(len(B33_norm[:,0,0])):
+   for j in range(nj):
+      for k in range(nk):
+         for n in range(nt):
+            match x:
+               case 1:
+                  B33_norm[x,j,k] = w_y_z_t_65[j,9,n]*w_y_z_t_65[j,k,n]/nt
+               case 2:
+                  B33_norm[x,j,k] = w_y_z_t_80[j,0,n]*w_y_z_t_80[j,k,n]/nt
+               case 3:
+                  B33_norm[x,j,k] = w_y_z_t_110[j,0,n]*w_y_z_t_110[j,k,n]/nt
+               case 4:
+                  B33_norm[x,j,k] = w_y_z_t_130[j,0,n]*w_y_z_t_130[j,k,n]/nt
+
+
+      B33_norm[x,j,:] = B33_norm[x,j,:]/B33_norm[x,j,0]
+      for p in range(nk):
+         if B33_norm[x,j,p] < 0:
+            B33_norm[x,j,p] = 0
+
+
+z = np.linspace(0,0.2,nk)
+x = [0.65,0.8,1.1,1.3]
+for fig in range(4):
+   plt.figure()
+   plt.subplots_adjust(left=0.20,bottom=0.20)
+   plt.plot(z,B33_norm[fig,0,:],"b")
+   plt.plot(z,B33_norm[fig,1,:],"r")
+   plt.plot(z,B33_norm[fig,2,:],"k")
+   plt.plot(z,B33_norm[fig,3,:],"g")
+   plt.plot(z,B33_norm[fig,4,:],"b-.")
+
+   plt.xlabel("z")
+   plt.ylabel("$B_{33}^{norm} ( x_3^A,\hat{x}_3)$")
+   plt.title(f"Two point correlation, x = {x[fig]}")
+   plt.savefig("Assignment 2/Assignment 2b/2point" + str(fig) + ".png")
 
 xy= np.loadtxt(name_dat + "hump_grid_nasa_les_coarse_noflow.dat")
 x1=xy[:,0]
@@ -141,7 +177,3 @@ for jj in range (0,nj):
 
       xp2d[ii,jj]=0.25*(x_2d[i,j]+x_2d[im1,j]+x_2d[i,jm1]+x_2d[im1,jm1])
       yp2d[ii,jj]=0.25*(y_2d[i,j]+y_2d[im1,j]+y_2d[i,jm1]+y_2d[im1,jm1])
-
-z = np.linspace(0,0.1,nk)
-
-
