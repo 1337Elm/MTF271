e95b94c3ba9c68909054d733d2b110e27a79c33c
benjamin <benjamin.elmjonsson@gmail.com>
2024-12-11 18:34:44 +0100

    2p corr plots fixed

--- Assignment 2/Assignment 2b/pl_twocorr_computed_fk.py
+++ Assignment 2/Assignment 2b/pl_twocorr_computed_fk.py
@@ -104,44 +104,76 @@ w_y_z_t_130= np.swapaxes(w_y_z_t,0,2) #nk,nj,nt
 w_y_z_t_130= np.swapaxes(w_y_z_t_130,0,1)
 
 x_pos = [0.65,0.8,1.1,1.3]
-B33_norm = np.zeros((len(x_pos),nj,nk))
+B33_norm065 = np.zeros((nj,nk))
+B33_norm08 = np.zeros((nj,nk))
+B33_norm11 = np.zeros((nj,nk))
+B33_norm13 = np.zeros((nj,nk))
 for x in range(len(x_pos)):
    for j in range(nj):
       for k in range(nk):
          for n in range(nt):
-            match x:
-               case 1:
-                  B33_norm[x,j,k] = B33_norm[x,j,k] + w_y_z_t_65[j,0,n]*w_y_z_t_65[j,k,n]/nt
-               case 2:
-                  B33_norm[x,j,k] = B33_norm[x,j,k] + w_y_z_t_80[j,0,n]*w_y_z_t_80[j,k,n]/nt
-               case 3:
-                  B33_norm[x,j,k] = B33_norm[x,j,k] + w_y_z_t_110[j,0,n]*w_y_z_t_110[j,k,n]/nt
-               case 4:
-                  B33_norm[x,j,k] = B33_norm[x,j,k] + w_y_z_t_130[j,0,n]*w_y_z_t_130[j,k,n]/nt
+               B33_norm065[j,k] += w_y_z_t_65[j,0,n]*w_y_z_t_65[j,k,n]/nt
+               B33_norm08[j,k] += w_y_z_t_80[j,0,n]*w_y_z_t_80[j,k,n]/nt
+               B33_norm11[j,k] += w_y_z_t_110[j,0,n]*w_y_z_t_110[j,k,n]/nt
+               B33_norm13[j,k] += w_y_z_t_130[j,0,n]*w_y_z_t_130[j,k,n]/nt
 
 
-      B33_norm[x,j,:] = B33_norm[x,j,:]/B33_norm[x,j,0]
+      B33_norm065[j,:] /= B33_norm065[j,0]
+      B33_norm08[j,:] /= B33_norm08[j,0]
+      B33_norm11[j,:] /= B33_norm11[j,0]
+      B33_norm13[j,:] /= B33_norm13[j,0]
+
       for p in range(nk):
-         if B33_norm[x,j,p] < 0:
-            B33_norm[x,j,p] = 0
+         if B33_norm065[j,p] < 0:
+            B33_norm065[j,p] = 0
+         if B33_norm08[j,p] < 0:
+            B33_norm08[j,p] = 0
+         if B33_norm11[j,p] < 0:
+            B33_norm11[j,p] = 0
+         if B33_norm13[j,p] < 0:
+            B33_norm13[j,p] = 0
 
 
 z = np.linspace(0,0.2,nk)
 for fig in range(4):
    plt.figure()
    plt.subplots_adjust(left=0.20,bottom=0.20)
-   plt.plot(z,B33_norm[fig,0,:],"b",label="$j=10$")
-   plt.plot(z,B33_norm[fig,1,:],"r",label="$j=30$")
-   plt.plot(z,B33_norm[fig,2,:],"k",label="$j=50$")
-   plt.plot(z,B33_norm[fig,3,:],"g",label="$j=70$")
-   plt.plot(z,B33_norm[fig,4,:],"b-.",label="$j=90$")
 
-   plt.ylim([0,1])
-   plt.xlim([z[0],z[-1]])
+   if fig == 0:
+      plt.plot(z,B33_norm065[0,:],"b",label="$j=10$")
+      plt.plot(z,B33_norm065[1,:],"r",label="$j=30$")
+      plt.plot(z,B33_norm065[2,:],"k",label="$j=50$")
+      plt.plot(z,B33_norm065[3,:],"g",label="$j=70$")
+      plt.plot(z,B33_norm065[4,:],"b-.",label="$j=90$")
+      plt.title(f"Two point correlation, x = {x_pos[fig]}")
+      plt.savefig("Assignment 2/Assignment 2b/2point" + str(x_pos[fig]) + ".png")
+   elif fig == 1:
+      plt.plot(z,B33_norm08[0,:],"b",label="$j=10$")
+      plt.plot(z,B33_norm08[1,:],"r",label="$j=30$")
+      plt.plot(z,B33_norm08[2,:],"k",label="$j=50$")
+      plt.plot(z,B33_norm08[3,:],"g",label="$j=70$")
+      plt.plot(z,B33_norm08[4,:],"b-.",label="$j=90$")
+   elif fig == 2:
+      plt.plot(z,B33_norm11[0,:],"b",label="$j=10$")
+      plt.plot(z,B33_norm11[1,:],"r",label="$j=30$")
+      plt.plot(z,B33_norm11[2,:],"k",label="$j=50$")
+      plt.plot(z,B33_norm11[3,:],"g",label="$j=70$")
+      plt.plot(z,B33_norm11[4,:],"b-.",label="$j=90$")
+   else:
+      plt.plot(z,B33_norm13[0,:],"b",label="$j=10$")
+      plt.plot(z,B33_norm13[1,:],"r",label="$j=30$")
+      plt.plot(z,B33_norm13[2,:],"k",label="$j=50$")
+      plt.plot(z,B33_norm13[3,:],"g",label="$j=70$")
+      plt.plot(z,B33_norm13[4,:],"b-.",label="$j=90$")
+
+   plt.xlim([z[0],z[-1]+0.01])
+   plt.legend(fontsize = "xx-small")
    plt.xlabel("z")
    plt.ylabel("$B_{33}^{norm} ( x_3^A,\hat{x}_3)$")
+
    plt.title(f"Two point correlation, x = {x_pos[fig]}")
-   plt.savefig("Assignment 2/Assignment 2b/2point" + str(fig) + ".png")
+   plt.savefig("Assignment 2/Assignment 2b/2point" + str(x_pos[fig]) + ".png")
+         
 
 xy= np.loadtxt(name_dat + "hump_grid_nasa_les_coarse_noflow.dat")
 x1=xy[:,0]
